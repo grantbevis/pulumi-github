55b7d47147629f2fe482844bb869cd1311e1aab2
b3vis <grant@bev.is>
2022-04-24 16:38:33 +0100

    Imported NixOS repo

--- __main__.py
+++ __main__.py
@@ -1,14 +1,35 @@
-"""A GitHub Python Pulumi program to create it's own repo"""
+"""A Python Pulumi program to manage my GitHub"""
 
 import pulumi
 import pulumi_github as github
 
-pulumi_github_repo = github.Repository("pulumi-github",
+grantbevis = github.Repository("grantbevis",
+    name="grantbevis",
+    description="My personal GitHub repo",
+    visibility="public")
+
+
+pulumi_github = github.Repository("pulumi-github",
     name="pulumi-github",
     description="My personal Pulumi GitHub configuration",
     visibility="public",
     has_wiki=False,
     has_issues=True)
 
+
+nixos = github.Repository("nixos",
+    allow_auto_merge=True,
+    default_branch="main",
+    has_downloads=True,
+    has_issues=True,
+    has_projects=True,
+    has_wiki=True,
+    name="nixos",
+    description="My personal NixOS repo ❄️",
+    visibility="private",
+    vulnerability_alerts=True,
+    opts=pulumi.ResourceOptions(protect=True))
+
+
 # Export the Name of the repository
-pulumi.export('name', pulumi_github_repo.name)
+pulumi.export('name', pulumi_github.name)

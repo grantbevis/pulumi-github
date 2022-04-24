c3a5b483bb07d23a64122ff4ea6ef7d96afe3106
b3vis <grant@bev.is>
2022-04-24 16:48:15 +0100

    working

--- __main__.py
+++ __main__.py
@@ -1,25 +1,33 @@
 """A Python Pulumi program to manage my GitHub"""
 
+from pip import main
 import pulumi
 import pulumi_github as github
 
 grantbevis = github.Repository("grantbevis",
+    allow_auto_merge=True,
+    has_downloads=True,
+    has_issues=True,
+    has_projects=True,
+    has_wiki=True,
     name="grantbevis",
     description="My personal GitHub repo",
-    visibility="public")
-
+    visibility="private",
+    vulnerability_alerts=True,
+    opts=pulumi.ResourceOptions(protect=True))
 
 pulumi_github = github.Repository("pulumi-github",
     name="pulumi-github",
     description="My personal Pulumi GitHub configuration",
     visibility="public",
     has_wiki=False,
-    has_issues=True)
+    has_issues=True,
+    opts=pulumi.ResourceOptions(protect=True))
 
-
+# pulumi import github:index/repository:Repository nixos nixos
 nixos = github.Repository("nixos",
     allow_auto_merge=True,
-    default_branch="main",
+    # default_branch="main",
     has_downloads=True,
     has_issues=True,
     has_projects=True,
@@ -29,6 +37,10 @@ nixos = github.Repository("nixos",
     visibility="private",
     vulnerability_alerts=True,
     opts=pulumi.ResourceOptions(protect=True))
+
+nixos_default_branch = github.BranchDefault("nixos_default_branch",
+    repository=nixos.name,
+    branch="main")
 
 
 # Export the Name of the repository
